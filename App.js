0a9ff5bec447cdd15960904c4919906201755fd8
mithu-sivali <mithu.sivali@gmail.com>
2024-04-06 17:14:42 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Pressable, ActivityIndicator } from 'react-native';
+import React, { useState } from "react";
+import axios from 'axios';
+import {OpenAI} from "openai";
 
-export default function App() {
+
+function App() {
+  const [prompt, setPrompt] = React.useState('');
+  const [response, setResponse] = React.useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const openai = new OpenAI(
+    {
+      apiKey: 'dummy-key',
+      dangerouslyAllowBrowser: true,
+    }
+  );
+  async function generateAnswer() {
+    setIsLoading(true);
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "You are a helpful assistant. " + prompt }],
+      model: "gpt-3.5-turbo",
+    });
+
+    console.log(completion.choices[0].message.content);
+    setResponse(completion.choices[0].message.content);
+
+    setIsLoading(false);
+
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={newText => setPrompt(newText)}
+        value={prompt}
+      />
+      <Pressable
+        title={isLoading ? "Generating..." : "Generate Convo"}
+        disabled={isLoading}
+        onPress={() => generateAnswer()}
+        style={styles.button}
+      >
+        <Text style={styles.buttontext}>{isLoading ? "Generating..." : "Generate Answer"}</Text>
+      </Pressable>
+      {isLoading && <Text style={styles.loadingText}>Loading...</Text>}
+      {isLoading && <ActivityIndicator size="large" />}
+      <Text>{response}</Text>
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +57,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  buttontext: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
 });
+
+export default App;
